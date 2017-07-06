1efed6e00fa16cf75faed5fc78258aa00d439bd2
PSP <cocoabono@gmail.com>
2017-07-06 15:33:25 +0900

    tsetset

--- tesetest/src/tesetest/test.java
+++ tesetest/src/tesetest/test.java
@@ -3,8 +3,8 @@ package tesetest;
 public class test {
 
 	public static void main(String[] args) {
-System.out.println("aaaa");
-
+		System.out.println("aaaa");
+		System.out.println("bbbb");
 	}
 
 }
